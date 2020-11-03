d4f114bee1c10d4e533e20598adf8fcb745a19a0
= <=>
2020-11-03 09:59:44 +0100

    feat: add menu bar

--- src/view/components/header.php
+++ src/view/components/header.php
@@ -1,16 +1,16 @@
-<div class="w-100 p-2 d-flex flex-row justify-content-between" style="background: linear-gradient(to right, #63c76a, #63c76a, #a2caa5);">
+<div id="header" class="w-100 p-2 flex flex-row justify-between items-center">
     <div>
-        <button type="button">
+        <button type="button" onclick="openMenuBar()">
             <!-- Burger Button -->
             <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-justify" fill="white" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
             </svg>
         </button>
         <button type="button">
-            <img src="../../../public/images/icon.png" alt="icon logo" style="width: 3rem;" />
+            <img class="application-icon" src="../../../public/images/icon.png" alt="icon logo" />
         </button>
     </div>
-    <h1 class="text-white font-weight-normal"> Menu </h1>
+    <h1 class="text-white text-2xl"> Menu </h1>
     <button type="button">
         <!-- Log Button -->
         <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-box-arrow-in-right" fill="white" xmlns="http://www.w3.org/2000/svg">
@@ -18,4 +18,53 @@
             <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
         </svg>
     </button>
+    <!-- Hidden menu bar -->
+    <div id="burgermenu" class="fixed top-0 -ml-4 w-1/2 lg:w-1/4 h-screen" >
+        <div class="flex justify-between">
+            <button type="button">
+                <img class="application-icon m-4" src="../../../public/images/icon.png" alt="icon logo" />
+            </button>
+            <button onclick="closeMenuBar()" class="m-4">
+                <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-x" fill="white" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                </svg>
+            </button>
+        </div>
+        <div class="flex flex-col">
+            <div class="flex flex-row items-center m-auto py-1">
+                <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-house" fill="white" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                    <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                </svg>
+                <a href="" class="link ml-2 text-white"> Accueil </a>
+            </div>
+            <div class="flex flex-row items-center m-auto py-1">
+                <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-plus" fill="white" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                </svg>
+                <a href="" class="link ml-2 text-white"> Poster un ticket </a>
+            </div>
+            <div class="flex flex-row items-center m-auto py-1">
+                <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-person" fill="white" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M10 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6 5c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                </svg>
+                <a href="" class="link ml-2 text-white" > Mon profil </a>
+            </div>
+            <div class="flex flex-row items-center m-auto py-1">
+                <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-envelope" fill="white" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
+                </svg>
+                <a href="" class="link ml-2 text-white"> Aide </a>
+            </div>
+            <div class="flex flex-row items-center m-auto py-1">
+                <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-box-arrow-in-right" fill="white" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
+                    <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                </svg>
+                <a href="" class="link ml-2 text-white"> Déconnexion </a>
+            </div>
+        </div>
+    </div>
 </div>
+
+<script type="text/javascript" src="../../../public/js/header.js"></script>
--- src/view/components/links.php
+++ src/view/components/links.php
@@ -1,31 +1,13 @@
 <!-- Include file into HTML Header -->
 
-
 <!-- favicon -->
 <link rel="icon" type="image/png" href="../../../public/images/icon.png" />
 
-<!-- Fontfaces CSS-->
-<link href="../../../public/css/font-face.css" rel="stylesheet" media="all">
-<link href="../../../public/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-<link href="../../../public/vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
-<link href="../../../public/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+<!-- Base CSS-->
+<link href="../../../public/scss/base.scss" rel="stylesheet" media="all">
 
-<!-- Bootstrap CSS-->
-<link href="../../../public/vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
-<!-- Online Boostrap CSS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-<!-- Vendor CSS-->
-<link href="../../../public/vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
-<link href="../../../public/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
-<link href="../../../public/vendor/wow/animate.css" rel="stylesheet" media="all">
-<link href="../../../public/vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
-<link href="../../../public/vendor/slick/slick.css" rel="stylesheet" media="all">
-<link href="../../../public/vendor/select2/select2.min.css" rel="stylesheet" media="all">
-<link href="../../../public/vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
-
-<!-- Main CSS-->
-<link href="../../../public/css/theme.css" rel="stylesheet" media="all">
+<!-- Tailwind CSS-->
+<link href="../../../public/scss/tailwind.scss" rel="stylesheet" media="all">
 
 <!-- Custom CSS -->
-<link href="../../../public/css/style.css" rel="stylesheet" media="all">
+<link href="../../../public/scss/style.scss" rel="stylesheet" media="all">
