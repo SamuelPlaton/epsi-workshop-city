20c1d77fa86369277578b746f61c2ae1b9990509
Fabien <bdnfabien@gmail.com>
2020-11-03 10:31:05 +0100

    AJOUT META IMPORTANTE RESPONSIVE

--- src/view/components/links.php
+++ src/view/components/links.php
@@ -11,3 +11,8 @@
 
 <!-- Custom CSS -->
 <link href="../../../public/scss/style.scss" rel="stylesheet" media="all">
+
+<meta name="Content-Type" content="UTF-8">
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0">
