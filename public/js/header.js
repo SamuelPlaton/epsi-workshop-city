4dcceb302b166b5e5b7b155bffc7dfdf1991c87e
= <=>
2020-11-03 10:12:26 +0100

    feat: add transparent background

--- public/js/header.js
+++ public/js/header.js
@@ -1,6 +1,9 @@
 function openMenuBar(){
     document.getElementById("burgermenu").style.transform = "translateX(0%)";
+    console.log('opened');
+    document.getElementById("hidebar").style.display = "block";
 }
 function closeMenuBar(){
     document.getElementById("burgermenu").style.transform = "translateX(-105%)";
+    document.getElementById("hidebar").style.display = "none";
 }
