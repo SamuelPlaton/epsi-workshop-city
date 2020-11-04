b12780ba9d9a00f56386452651cec73b4a6f6eb1
= <=>
2020-11-04 11:26:17 +0100

    feat: add upload image section

--- src/model/createTicketModel.php
+++ src/model/createTicketModel.php
@@ -1,4 +1,5 @@
 <?php
+
 require('../controller/createTicketController.php');
 $bdd = new PDO('mysql:host=localhost;dbname=cityplus;charset=utf8', 'root', '');
 $categories = $bdd->query('SELECT * FROM TICKET_CATEGORIES');
--- src/view/components/header.php
+++ src/view/components/header.php
@@ -38,13 +38,13 @@
                     <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
                     <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
                 </svg>
-                <a href="/src/view/homeView.php" class="link ml-2 text-white"> Accueil </a>
+                <a href="/city+/src/model/homeModel.php" class="link ml-2 text-white"> Accueil </a>
             </div>
             <div class="flex flex-row items-center m-auto py-1">
                 <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-plus" fill="white" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                 </svg>
-                <a href="/src/view/createTicketView.php" class="link ml-2 text-white"> Poster un ticket </a>
+                <a href="/city+/src/model/createTicketModel.php" class="link ml-2 text-white"> Poster un ticket </a>
             </div>
             <div class="flex flex-row items-center m-auto py-1">
                 <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-person" fill="white" xmlns="http://www.w3.org/2000/svg">
