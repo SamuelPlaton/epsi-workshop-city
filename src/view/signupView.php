d399caed10e18fef3fe5bd8e5c864cc5c74dee5d
Papergoal <etienne.manceau@epsi.fr>
2020-11-04 11:32:54 +0100

    feat: add sign up view

--- src/view/signupView.php
+++ src/view/signupView.php
@@ -26,12 +26,17 @@
         <input type="text" id="phoneNumber" name="phoneNumber" placeholder="+33 6 ..." class="form-control w-full mx-4 mx-auto border-solid border-2 border-gray rounded-lg p-1" required>
     </div>
     <div class="form-group flex flex-col my-2">
-        <label for="age" class="form-control-label text-lg mb-2">Âge</label>
-        <input type="text" id="age" name="age" placeholder=" (Optionnel) " class="form-control w-full mx-4 mx-auto border-solid border-2 border-gray rounded-lg p-1" >
+        <label for="birth" class="form-control-label text-lg mb-2">Date de naissance</label>
+        <input type="date" id="birth" name="birth" placeholder=" " class="form-control w-full mx-4 mx-auto border-solid border-2 border-gray rounded-lg p-1" >
     </div>
     <div class="form-group flex flex-col my-2">
         <label for="gender" class="form-control-label text-lg mb-2">Sexe</label>
-        <input type="text" id="gender" name="gender" placeholder=" (Optionnel) " class="form-control w-full mx-4 mx-auto border-solid border-2 border-gray rounded-lg p-1" >
+        <label for="male">Homme</label>
+            <input type="radio" name="gender" value="male" class="lg:w-1/3" checked> <br>
+        <label for="female">Femme</label>
+            <input type="radio" name="gender" value="female" class="lg:w-1/3"> <br>
+        <label for="other">Autre</label>
+            <input type="radio" name="gender" value="other" class="lg:w-1/3">
     </div>
     <div class="form-group flex flex-col my-2">
         <label for="mailAddress" class="form-control-label text-lg mb-2">Mail</label>
@@ -39,12 +44,12 @@
     </div>
     <div class="form-group flex flex-col my-2">
         <label for="title" class="form-control-label text-lg mb-2">Mot de passe</label>
-        <input type="password" id="typepass" name="password" placeholder=" " class="form-control w-full mx-4 mx-auto border-solid border-2 border-gray rounded-lg p-1" required>
+        <div class="flex flex-row justify-between w-full mx-4 mx-auto">
+            <input type="password" id="typepass" name="password" placeholder=" " class="form-control flex-grow border-solid border-2 border-gray rounded-lg p-1" required>
+            <input type="image" src="../../public/images/view-show.svg" class="mx-2 w-6" onclick="Toggle1()">
+        </div>
     </div>
     <div>
-        <input type="checkbox" onclick="Toggle1()">
-        <b>Voir le mot de passe</b>
-
         <script>
             // Change the type of input to password or text
             function Toggle1() {
@@ -61,12 +66,12 @@
     <br>
     <div class="form-group flex flex-col my-2">
         <label for="title" class="form-control-label text-lg mb-2">Entrez le mot de passe à nouveau</label>
-        <input type="password" id="typepass2" name="password2" placeholder=" " class="form-control w-full mx-4 mx-auto border-solid border-2 border-gray rounded-lg p-1" required>
+        <div class="flex flex-row justify-between w-full mx-4 mx-auto">
+        <input type="password" id="typepass2" name="password2" placeholder=" " class="form-control flex-grow border-solid border-2 border-gray rounded-lg p-1" required>
+        <input type="image" src="../../public/images/view-show.svg" class="mx-2 w-6" onclick="Toggle2()">
+        </div>
     </div>
     <div>
-        <input type="checkbox" onclick="Toggle2()">
-        <b>Voir le mot de passe</b>
-
         <script>
             // Change the type of input to password or text
             function Toggle2() {
@@ -90,6 +95,9 @@
     </div>
     <div class="text-center mt-5">
         <br>
+        <button  id="accountOwned" name="accountOwned" type="button" class="text-white py-2 px-4 rounded w-50 m-auto text-lg" style="background-color: #2980b9;">
+            J'ai déjà un compte
+        </button>
         <button id="sign-up-button" name="sign-up-button" type="submit" class="text-white py-2 px-4 rounded w-50 m-auto text-lg" style="background-color: #63c76a;">
             Créer mon compte
         </button>
